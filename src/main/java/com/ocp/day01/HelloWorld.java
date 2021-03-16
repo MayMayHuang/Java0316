6cde353430a879c86bb1243f715f381f2fafd3f2
MayMayHuang <73887234+MayMayHuang@users.noreply.github.com>
2021-03-16 20:33:30 +0800

    Update HelloWorld.java

--- src/main/java/com/ocp/day01/HelloWorld.java
+++ src/main/java/com/ocp/day01/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Java 的第一堂課");
         System.out.println("Hello Java World");
-        
+        System.out.println("Hello Java World");
     }
  
 }
