db14376695f0a1c756bb9ce5d6bd2253d7c2d273
MB-study <MB-study@192.168.1.93>
2021-03-23 21:50:42 +0800

    java

--- src/main/java/com/ocp/day06/ArrayDemo2.java
+++ src/main/java/com/ocp/day06/ArrayDemo2.java
@@ -3,26 +3,35 @@ package com.ocp.day06;
 import java.util.Arrays;
 
 public class ArrayDemo2 {
+
     public static void main(String[] args) {
-        int[] scores = {50,90,90,80,70,100,40,100,75,15};
-        System.out.printf("%s, 長度: %d\n", Arrays.toString(scores),scores.length);
+        int[] scores = {50, 90, 90, 80, 70, 100, 40, 100, 75, 15};
+        System.out.printf("%s, 長度: %d\n", Arrays.toString(scores), scores.length);
         //總分?
         int sum = 0;
-        for(int i=0; i<scores.length;i++){//不可以等於
-             sum += scores[i];//累計,-=累減
+        for (int i = 0; i < scores.length; i++) {//不可以等於
+            sum += scores[i];//累計,-=累減
         }
         //平均?  
         double avg = (double) sum / scores.length;
-        System.out.printf("總分: %d 平均: %.1f\n",sum,avg);
+        System.out.printf("總分: %d 平均: %.1f\n", sum, avg);
         //求最高分?
         int max = 0;
-        
-        
+        for (int i = 0; i < scores.length; i++) {//替換法
+            if (scores[i] > max) {
+                max = scores[i];
+            }
         }
-        
-        
- 
-                
-                
-    
+        System.out.printf("最高分: %d\n", max);
+        //最低分
+        int min = 100;
+        for (int i = 0; i < scores.length; i++) {//替換法
+            if (scores[i] < min) {
+                min = scores[i];
+            }
+        }
+        System.out.printf("最低分: %d\n", min);
+
+    }
+
 }
