36bb5153df6b8c2a1c2afe1a7d2f8fcb244a7023
MB-study <MB-study@192.168.1.93>
2021-04-06 20:32:40 +0800

    java

--- src/main/java/com/ocp/day08/MultiArrayDemo4.java
+++ src/main/java/com/ocp/day08/MultiArrayDemo4.java
@@ -2,16 +2,13 @@ package com.ocp.day08;
 
 public class MultiArrayDemo4 {
     public static void main(String[] args) {
-        //3人的身高與體重如下:
-        double[][] hw = {{170,160,180},{60,45,90}};
-        //求3人的BMI(Homework)
-        for(double[] x : hw) {
-            for(double y: x) {
-            double h = x[0];
-            double w = x[1];
-           double bmi = w/Math.pow(h/100, 2);
-           System.out.printf("BMI: %.2f\n", bmi);
-           }
+        double[][] hw = {{170, 160, 180}, {60, 45, 90}};
+        double[] h = hw[0];
+        double[] w = hw[1];
+
+        for (int i = 0; i < 3; i++) {
+            double bmi = w[i] / Math.pow(h[i] / 100, 2);
+            System.out.printf("%.2f\n", bmi);
         }
          
         
