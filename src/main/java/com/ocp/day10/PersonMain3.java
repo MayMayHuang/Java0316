15d10e3ccf7749b3f5577f2f9310ae39f79d39c4
MB-study <MB-study@192.168.1.93>
2021-04-08 20:59:47 +0800

    java

---
+++ src/main/java/com/ocp/day10/PersonMain3.java
@@ -0,0 +1,13 @@
+package com.ocp.day10;
+
+public class PersonMain3 {
+    Person[] persons = {//陣列
+            new Person("John", 17, 170, 60.5),
+            new Person("Mary", 18, 160, 50.5),
+            new Person("Jo", 19, 155, 42.5),
+            new Person("Helen", 16, 185, 75.5),
+            new Person("Tom", 21, 190, 120.5),
+        };
+        //計算bmi的平均,最大與最小各為多少?
+    
+}
