bff6cf73bf93d7431bc7a387dc6901352550c0d5
MB-study <MB-study@192.168.1.93>
2021-05-04 21:34:08 +0800

    java

---
+++ src/main/java/com/ocp/day22/Lotto.java
@@ -0,0 +1,30 @@
+package com.ocp.day22;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Lotto {
+    private String value;
+    
+    private static Lotto lottoInstance = new Lotto();
+    public static Lotto getLotto() {
+        return lottoInstance;
+    }
+    
+    private Lotto(){
+        Random r = new Random();
+        int [] values = {
+            r.nextInt(9) + 1,
+            r.nextInt(9) + 1,
+            r.nextInt(9) + 1,
+            r.nextInt(9) + 1
+        }; 
+        value = Arrays.toString(values);
+      }
+      public String getValue(){
+          return value;
+      }
+        
+    }
+    
+
