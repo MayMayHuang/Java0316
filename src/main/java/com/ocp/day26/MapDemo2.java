c89208c339b413329f8b11317c911bcb94046f1b
User <User@DESKTOP-97PQ5OT>
2021-05-20 18:58:25 +0800

    Java

---
+++ src/main/java/com/ocp/day26/MapDemo2.java
@@ -0,0 +1,27 @@
+package com.ocp.day26;
+
+import java.util.IntSummaryStatistics;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapDemo2 {
+    public static void main(String[] args) {
+        Map<String, Integer> exams = new LinkedHashMap<>();
+        exams.put("國文",100);//("國文,100) -> 稱為Entry(含有Key/Value)
+        exams.put("數學",100);
+        exams.put("英文",90);
+        exams.put("英文",80);//map相同key值,後蓋前
+        System.out.println(exams);
+        //有很多Entry我們稱為EntrySet
+        
+        exams.entrySet().forEach(entry ->System.out.println(entry.getKey()));
+        exams.entrySet().forEach(entry ->System.out.println(entry.getValue()));
+        IntSummaryStatistics stat = exams.entrySet()
+                .stream()
+                .mapToInt(entry ->entry.getValue())
+                .summaryStatistics();
+        System.out.println(stat.getSum());
+        System.out.println(stat.getAverage());
+    }
+    
+}
