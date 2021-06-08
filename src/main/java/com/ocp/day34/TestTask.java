cddfbbe53f9af195b2091a3823847b890c90a27a
User <User@DESKTOP-97PQ5OT>
2021-06-08 21:20:57 +0800

    Java

--- src/main/java/com/ocp/day34/TestTask.java
+++ src/main/java/com/ocp/day34/TestTask.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-class Task implements Runnable {
+class Task implements Runnable {//短任務
 
     @Override
     public void run() {
@@ -12,13 +12,13 @@ class Task implements Runnable {
     }
     
 }
-class LongTask implements Runnable {
+class LongTask implements Runnable {//長任務
 
     @Override
     public void run() {
         System.out.println("長任務開始");
         try {
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.SECONDS.sleep(5);//執行長任務中...
         } catch(Exception e) {
             
         }
@@ -28,14 +28,17 @@ class LongTask implements Runnable {
 }
 
 public class TestTask {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         ScheduledExecutorService service = Executors.newScheduledThreadPool(4);
         service.submit(new Task());
         service.submit(new Task());
         service.submit(new LongTask());
         service.submit(new Task());
+        
         service.shutdown();
         
+
+        //偵測pool有沒有關閉
         while(!service.awaitTermination(1,TimeUnit.SECONDS)) {
             System.out.println("Pool not close...");
         }
